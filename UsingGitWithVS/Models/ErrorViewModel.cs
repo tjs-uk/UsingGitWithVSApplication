a1af67c8de20453fa99ec4d9a1bfabba20a2a22b
Trevor Smith <104841977+tjs-cap@users.noreply.github.com>
2022-06-21 13:57:14 +0100

    Add project files.

---
+++ UsingGitWithVS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UsingGitWithVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
